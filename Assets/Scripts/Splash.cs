baa74997f361d0fe795441ab34e46c2ee15946da
Ronald Davis <ronnied1222@msn.com>
2018-05-31 14:01:34 -0400

    updates

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour {
+
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadNextLevel", 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadNextLevel(){
+        SceneManager.LoadScene(1);
+    }
+}
